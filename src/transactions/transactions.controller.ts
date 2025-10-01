528586acf8c6b42648397cbb17b10d1ef79d172b
shem958 <shemgikunda37@gmail.com>
2025-10-01 16:39:12 +0300

    feat: implement TransactionsController with CRUD operations for transaction management

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+    constructor(private readonly transactionsService: TransactionsService) { }
+
+    @Post()
+    create(@Body() createTransactionDto: any) {
+        return this.transactionsService.create(createTransactionDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.transactionsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.transactionsService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateTransactionDto: any) {
+        return this.transactionsService.update(id, updateTransactionDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.transactionsService.remove(id);
+    }
+}
