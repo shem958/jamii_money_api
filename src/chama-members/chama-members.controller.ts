0bd703f944257c45aa7d485998a21e817c3e0f10
shem958 <shemgikunda37@gmail.com>
2025-10-01 16:52:32 +0300

    feat: implement ChamaMembersController with CRUD operations for chama member management

--- src/chama-members/chama-members.controller.ts
+++ src/chama-members/chama-members.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { ChamaMembersService } from './chama-members.service';
 
 @Controller('chama-members')
-export class ChamaMembersController {}
+export class ChamaMembersController {
+    constructor(private readonly chamaMembersService: ChamaMembersService) { }
+
+    @Post()
+    create(@Body() createMemberDto: any) {
+        return this.chamaMembersService.create(createMemberDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.chamaMembersService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.chamaMembersService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateMemberDto: any) {
+        return this.chamaMembersService.update(id, updateMemberDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.chamaMembersService.remove(id);
+    }
+}
