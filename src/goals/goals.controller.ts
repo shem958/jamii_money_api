dfa5e2914a5ea04f5f4075ad68d7d2bef5956429
shem958 <shemgikunda37@gmail.com>
2025-10-01 16:40:45 +0300

    feat: implement GoalsController with CRUD operations for goal management

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { GoalsService } from './goals.service';
 
 @Controller('goals')
-export class GoalsController {}
+export class GoalsController {
+    constructor(private readonly goalsService: GoalsService) { }
+
+    @Post()
+    create(@Body() createGoalDto: any) {
+        return this.goalsService.create(createGoalDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.goalsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.goalsService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateGoalDto: any) {
+        return this.goalsService.update(id, updateGoalDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.goalsService.remove(id);
+    }
+}
