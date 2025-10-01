6a1637943b54b8745528d1c05677f9e2156bfd48
shem958 <shemgikunda37@gmail.com>
2025-10-01 16:54:35 +0300

    feat: implement NudgesController with CRUD operations for nudge management

--- src/nudges/nudges.controller.ts
+++ src/nudges/nudges.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { NudgesService } from './nudges.service';
 
 @Controller('nudges')
-export class NudgesController {}
+export class NudgesController {
+    constructor(private readonly nudgesService: NudgesService) { }
+
+    @Post()
+    create(@Body() createNudgeDto: any) {
+        return this.nudgesService.create(createNudgeDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.nudgesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.nudgesService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateNudgeDto: any) {
+        return this.nudgesService.update(id, updateNudgeDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.nudgesService.remove(id);
+    }
+}
