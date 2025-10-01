716b7f0a3abaeafebccd99c37199c3f35a979883
shem958 <shemgikunda37@gmail.com>
2025-10-01 16:38:38 +0300

    feat: implement TransactionsService with CRUD operations for transaction management

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+    async create(createTransactionDto: any) {
+        return { message: 'Transaction created', data: createTransactionDto };
+    }
+
+    async findAll() {
+        return { message: 'All transactions', data: [] };
+    }
+
+    async findOne(id: string) {
+        return { message: `Transaction ${id}`, data: {} };
+    }
+
+    async update(id: string, updateTransactionDto: any) {
+        return { message: `Transaction ${id} updated`, data: updateTransactionDto };
+    }
+
+    async remove(id: string) {
+        return { message: `Transaction ${id} deleted` };
+    }
+}
