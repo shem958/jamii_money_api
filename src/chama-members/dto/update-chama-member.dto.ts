26ef5e96408d5a12025336edc7d75dc3112b28a0
shem958 <shemgikunda37@gmail.com>
2025-10-08 09:51:47 +0300

    feat: create UpdateChamaMemberDto extending CreateChamaMemberDto for partial updates

---
+++ src/chama-members/dto/update-chama-member.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateChamaMemberDto } from './create-chama-member.dto';
+
+export class UpdateChamaMemberDto extends PartialType(CreateChamaMemberDto) { }
