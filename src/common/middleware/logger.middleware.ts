7accfca204811d6af1021ab8a0224aec874c2ea2
shem958 <shemgikunda37@gmail.com>
2025-10-08 17:50:14 +0300

    refactor: update jest-e2e configuration for improved module resolution and transform settings

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import * as chalk from 'chalk';
+import chalk from 'chalk';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
