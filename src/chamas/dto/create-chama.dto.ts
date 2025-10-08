5c64128082fce82f3ba368330c0a273074b34d6d
shem958 <shemgikunda37@gmail.com>
2025-10-08 09:48:45 +0300

    feat: create CreateChamaDto with validation for name, description, members, and createdBy

---
+++ src/chamas/dto/create-chama.dto.ts
@@ -0,0 +1,19 @@
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateChamaDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsArray()
+    @IsOptional()
+    members?: string[];
+
+    @IsString()
+    @IsOptional()
+    createdBy?: string;
+}
