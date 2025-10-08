3a47579455d61a3c6cb91d796f4179598a2f3180
shem958 <shemgikunda37@gmail.com>
2025-10-08 09:40:59 +0300

    feat: create CreateWalletDto with validation for userId, balance, and currency

---
+++ src/wallets/dto/create-wallet.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateWalletDto {
+    @IsString()
+    @IsNotEmpty()
+    userId: string;
+
+    @IsNumber()
+    @IsOptional()
+    balance?: number;
+
+    @IsString()
+    @IsOptional()
+    currency?: string;
+}
