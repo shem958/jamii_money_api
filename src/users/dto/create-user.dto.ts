ddc8e0549f95404977383f4c506d283aa44169ce
shem958 <shemgikunda37@gmail.com>
2025-10-08 09:40:13 +0300

    feat: add DTOs for user and wallet management with validation

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    password?: string;
+
+    @IsString()
+    @IsOptional()
+    role?: string;
+}
