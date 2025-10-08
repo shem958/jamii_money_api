8481e58ae164da37b37c9352953995aaa8c6b0a6
shem958 <shemgikunda37@gmail.com>
2025-10-08 09:46:10 +0300

    feat: create CreateGoalDto with validation for userId, title, targetAmount, and deadline

---
+++ src/goals/dto/create-goal.dto.ts
@@ -0,0 +1,19 @@
+import { IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateGoalDto {
+    @IsString()
+    @IsNotEmpty()
+    userId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNumber()
+    @IsOptional()
+    targetAmount?: number;
+
+    @IsDateString()
+    @IsOptional()
+    deadline?: Date;
+}
